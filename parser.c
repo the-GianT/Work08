ecd9b4e21813be8484da8818600f69e293c2c3cb
Gian Tricarico <gtricarico@stuy.edu>
2018-04-16 11:46:31 -0400

    Add push and pop commands to parser

--- parser.c
+++ parser.c
@@ -22,39 +22,48 @@ The file follows the following format:
      Every command is a single character that takes up a line
      Any command that requires arguments must have those arguments in the second line.
      The commands are as follows:
-         sphere: add a sphere to the edge matrix - 
-	    takes 4 arguments (cx, cy, cz, r)
-         torus: add a torus to the edge matrix - 
-	    takes 5 arguments (cx, cy, cz, r1, r2)
-         box: add a rectangular prism to the edge matrix - 
-	    takes 6 arguments (x, y, z, width, height, depth)
 
-         circle: add a circle to the edge matrix - 
-	    takes 4 arguments (cx, cy, cz, r)
-	 hermite: add a hermite curve to the edge matrix -
-	    takes 8 arguments (x0, y0, x1, y1, rx0, ry0, rx1, ry1)
-	 bezier: add a bezier curve to the edge matrix -
-	    takes 8 arguments (x0, y0, x1, y1, x2, y2, x3, y3)
-         line: add a line to the edge matrix - 
-	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-	 ident: set the transform matrix to the identity matrix - 
-	 scale: create a scale matrix, 
-	    then multiply the transform matrix by the scale matrix - 
-	    takes 3 arguments (sx, sy, sz)
-	 move: create a translation matrix, 
-	    then multiply the transform matrix by the translation matrix - 
-	    takes 3 arguments (tx, ty, tz)
-	 rotate: create an rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 2 arguments (axis, theta) axis should be x y or z
-	 apply: apply the current transformation matrix to the 
-	    edge matrix
-	 display: draw the lines of the edge matrix to the screen
-	    display the screen
-	 save: draw the lines of the edge matrix to the screen
-	    save the screen to a file -
-	    takes 1 argument (file name)
-	 quit: end parsing
+     push: push a copy of the curent top of the coordinate system stack to the stack
+
+     pop: remove the current top of the coordinate system stack
+
+     All the shape commands work as follows:
+        1) Add the shape to a temporary matrix
+        2) Multiply that matrix by the current top of the coordinate system stack
+        3) Draw the shape to the screen
+        4) Clear the temporary matrix
+
+     sphere: add a sphere -
+             takes 4 arguments (cx, cy, cz, r)
+     torus: add a torus to the polygon matrix -
+            takes 5 arguments (cx, cy, cz, r1, r2)
+     box: add a rectangular prism -
+          takes 6 arguments (x, y, z, width, height, depth)
+     circle: add a circle -
+             takes 4 arguments (cx, cy, cz, r)
+     hermite: add a hermite curve -
+              takes 8 arguments (x0, y0, x1, y1, rx0, ry0, rx1, ry1)
+     bezier: add a bezier curve -
+             takes 8 arguments (x0, y0, x1, y1, x2, y2, x3, y3)
+     line: add a line to the edge matrix -
+           takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+
+     scale: create a scale matrix,
+            then multiply the current top of the coordinate system stack -
+            takes 3 arguments (sx, sy, sz)
+     translate: create a translation matrix,
+                then multiply the current top of the coordinate system stack -
+                takes 3 arguments (tx, ty, tz)
+     rotate: create a rotation matrix,
+             then multiply the transform matrix by the translation matrix -
+             takes 2 arguments (axis, theta) axis should be x, y or z
+
+     display: display the screen
+
+     save: save the screen to a file -
+           takes 1 argument (file name)
+
+    quit: end parsing
 
 See the file script for an example of the file format
 
